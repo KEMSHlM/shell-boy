9cd1e7f7cd1d744d884fae193c594cf504b215e9
kenshin <koyama.kenshin.748@s.kyushu-u.ac.jp>
2022-11-19 18:21:42 +0900

    added discription

--- scripts/ITO.sh
+++ scripts/ITO.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# 実行時にこのシェルがある場所で実行される．非推奨らしいが動くのでよし
 cd `dirname $0` || exit
 
 # 割れる数字にしよう
@@ -12,20 +13,28 @@ do
     rm ./"$i"/5atm.dat
     rm ./"$i"/multirun.sh.o*
 
+    # $i というディレクトリがなければ生成．
     # if [ 処理 ]; then -> ifの前後はスペースがいる
     if [ ! -d "$i" ]; then
         mkdir "$i"
     fi
     start=$((290+i*iter))
     end=$((start+iter-1))
+    # 0 ディレクトリのshell scriptをコピー
     cp ../bin/exe ./"$i"/ 
-    cp ./0/multirun.sh ./"$i"/ 
+    cp ./0/multirun.sh ./"$i"/
+
+    # ここでやっているのは，先ほどコピーしたshell scriptの一部を書き換えている．
+    # 6行目の48を24に置換，13行目をファイル名に応じて置換． 
     # iオプションをつけないと保存されない
     sed -i.bak -e "6 s/48/24/gp" -e "13 s/290 1 309/${start} 1 ${end}/gp" ./"$i"/multirun.sh
+    # 使用なのか，私の環境で起きる現象なのかわからないが，上のコマンドは置換ではなく，変更した行を下に複製してしまう．
+    # そこで，複製前の行を削除している．
     sed -i.bak -e "7d" -e "14d" ./"$i"/multirun.sh
     rm "$i"/*.bak
 
     cd "$i" || exit
+    # 変更した内容で別のshell scriptを実行．ここでコンパイルして実行するのもいいね
     pjsub multirun.sh
     cd ..
 done
--- scripts/cat.sh
+++ scripts/cat.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# 実行時にこのシェルがある場所で実行される．非推奨らしいが動くのでよし
 cd `dirname $0` || exit
 
+# マージしてできる生成物
 filename="1atm.dat"
+# 各階層にあるマージされるファイル
 margefile="5atm.dat"
+# 一時保存用ファイル
 tempfile="temp.dat"
+# 階層の数，ここでは0-49の50個を想定している
 N=50
 
 rm $filename
@@ -14,8 +19,11 @@ echo "start"
 
 for i in $(eval "echo {0..$((N-1))}")
 do
+    # ./$i/$magefile を$filenameに書き足す
     cat $filename "$i"/$margefile > $tempfile
+    # tempfileに一時保存．
     cp $tempfile $filename
+    # tempfileを削除．
     rm $tempfile
 done
 
